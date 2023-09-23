65475bfcf90485892b786577498500c32f558f4b
MilesCranmer <miles.cranmer@gmail.com>
2023-09-23 18:58:00 +0100

    Refactor unittests

--- test/unittests.jl
+++ test/unittests.jl
@@ -1,26 +1,31 @@
-using Test: @test, @inferred
+using Test: @test, @testset, @inferred
 using BaseType: base_numeric_type
 using DualNumbers: DualNumbers
 using DynamicQuantities: DynamicQuantities
 using Measurements: ±
 using Unitful: Unitful
 
-expected_type_pairs = [
-    Float32 => Float32,
-    ComplexF64 => Float64,
-    DualNumbers.Dual{Int64} => Int64,
-    DynamicQuantities.Quantity{Float32} => Float32,
-    typeof(1.5DynamicQuantities.u"km/s") => Float64,
-    typeof(1.5f0Unitful.u"km/s") => Float32,
-    BigFloat => BigFloat,
-    typeof(1.5 ± 0.2) => Float64,
-    typeof(1.5f0 ± 0.2f0) => Float32,
-]
+@testset "Basic usage" begin
+    expected_type_pairs = [
+        Float32 => Float32,
+        ComplexF64 => Float64,
+        DualNumbers.Dual{Int64} => Int64,
+        DynamicQuantities.Quantity{Float32} => Float32,
+        typeof(1.5DynamicQuantities.u"km/s") => Float64,
+        typeof(1.5f0Unitful.u"km/s") => Float32,
+        BigFloat => BigFloat,
+        typeof(1.5 ± 0.2) => Float64,
+        typeof(1.5f0 ± 0.2f0) => Float32,
+    ]
 
-for (x, y) in expected_type_pairs
-    @eval @test base_numeric_type($x) == $y
-    # Make sure compiler can inline it:
-    @eval @inferred $y base_numeric_type($x)
+    for (x, y) in expected_type_pairs
+        @eval @test base_numeric_type($x) == $y
+        # Make sure compiler can inline it:
+        @eval @inferred $y base_numeric_type($x)
+    end
+
+    @test base_numeric_type(1.5DynamicQuantities.u"km/s") == base_numeric_type(typeof(1.5DynamicQuantities.u"km/s"))
+    @inferred base_numeric_type(1.5DynamicQuantities.u"km/s")
 end
 
 @test base_numeric_type(1.5DynamicQuantities.u"km/s") == base_numeric_type(typeof(1.5DynamicQuantities.u"km/s"))
